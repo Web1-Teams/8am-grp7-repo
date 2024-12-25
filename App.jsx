dfd5e2b0b4af4912ef5e354a811647ed079c5538
masa shtaiwe <s12323123@stu.najah.edu>
2024-12-25 14:01:32 +0200

    صفحه الاداب باستخدام الرياكت
    اخر تعديل عليها باستخدام البوستراب مع الرياكت

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Languages from './components/Languages';
+import './components/Languages.css'
+function App() {
+  return (
+    <div className="App">
+      <Languages />
+    </div>
+  );
+}
+
+export default App;
